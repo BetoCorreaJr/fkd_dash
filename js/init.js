b71f567ea0d6cf2876ba71fd89823eb83c57cc96
Beto Jr <albertoscjunior@gmail.com>
2016-05-02 18:40:16 -0300

    Relatorio e contato

--- js/init.js
+++ js/init.js
@@ -4,5 +4,10 @@
       belowOrigin: true, // Displays dropdown below the button
       alignment: 'left' // Displays dropdown with edge aligned to the left of button
     });
+    $('.datepicker').pickadate({
+      selectMonths: true, // Creates a dropdown to control month
+      selectYears: 15 // Creates a dropdown of 15 years to control year
+  });
+    $('select').material_select();
   });
 
